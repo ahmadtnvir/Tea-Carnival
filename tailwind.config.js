7d754a6404aa315afc55569f2e51ca01f025de74
Tanvir Ahmad <tnvir9@gmail.com>
2024-08-20 11:49:09 +0600

    The feedback section is complete and now it is responsive for all device.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,22 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    // extend: {
+    //   maskImage: {
+    //     'custom-mask': '-webkit-linear-gradient(top, transparent, transparent 0, white 10%, white 80%, transparent 100%)',
+    //   },
+    // },
   },
   plugins: [],
+  // plugins: [
+  //   function({ addUtilities }) {
+  //     const newUtilities = {
+  //       '.mask-box-image-custom': {
+  //         '-webkit-mask-box-image': '-webkit-linear-gradient(top, transparent, transparent 0, white 10%, white 80%, transparent 100%)',
+  //       },
+  //     }
+
+  //     addUtilities(newUtilities, ['responsive', 'hover'])
+  //   },
+  // ],
 };
